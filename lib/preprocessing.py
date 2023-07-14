d366cb04f9723fe24c74196357025e6bc9d45965
Nelson Lojo <nelson.lojo@berkeley.edu>
2023-07-14 21:05:57 +0000

    improvements to loading and saving data

--- lib/preprocessing.py
+++ lib/preprocessing.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 
 def load_sample(percentage: float = 0.1,
-                        csv_file: str = "data/train_full.csv",
-                        sql_db: str = None
-                        ) -> pd.DataFrame:
+                csv_file: str = "data/train_full.csv",
+                sql_db: str = None
+                ) -> pd.DataFrame:
     """Load a sample from a data store"""
 
     df: pd.DataFrame = None
@@ -17,3 +17,14 @@ def load_sample(percentage: float = 0.1,
         df = pd.read_csv(csv_file)
 
     return df.sample(frac=percentage, axis=0).reset_index(drop=True)
+
+def save_data(df: pd.DataFrame,
+              csv_file: str = "saved.csv", 
+              sql_db: str = None
+              ) -> None:
+    
+    if sql_db is not None:
+        with sql3.connect(sql_db) as conn:
+            df.to_sql("data", conn)
+    else:
+        df.to_csv(csv_file)
